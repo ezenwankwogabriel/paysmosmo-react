76de95349888ed125751de53121e003eabfc3674
Gabriel <dagabangel@gmail.com>
2019-07-04 13:17:54 +0100

    Added styles and setup start script for scss

---
+++ src/components/auth/SignUp.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <React.Fragment>
+                
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default SignUp;
---
+++ src/components/sharedElements/Input.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Input = () => {
+    return ( <input type="text" placeholder="" name="" disabled="disabled"/> );
+}
+ 
+export default Input;
